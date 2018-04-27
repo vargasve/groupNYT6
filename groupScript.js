bc026c34b29bcbb1248b1feeb2763faab8e7f3ab
Wonbok Yang <won-83@hotmail.com>
2018-04-27 13:45:29 -0500

    fixed dates

--- groupScript.js
+++ groupScript.js
@@ -3,16 +3,25 @@ $(document).ready(function () {
     $("#search").on("click", function (event) {   //click button 
         event.preventDefault();
         console.log("click");
-      
+
 
         var searchTerm = $("#search-term").val().trim();
-        var startYear = $("#start-year").val().trim();
-        var endYear = $("#end-year").val().trim();
+        var startYear;
+        var endYear;
+
+
+
+
+
+
+
+       
+
+
+
 
 
         var limit = $("#article-count").val().trim();
-
-
 
         console.log("searchTerm: " + searchTerm);
         console.log("startYear: " + startYear);
@@ -23,10 +32,19 @@ $(document).ready(function () {
         var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         url += '?' + $.param({
             'api-key': "da7be3a4e5494580bc887c4a0b3c13de",
-            'q': searchTerm,
-            'begin_date': startYear,
-            'end_date': endYear
+            'q': searchTerm
         });
+
+        if ($("#end-year").val().trim()) {
+            url += "&end_date="+$("#end-year").val().trim();
+        }
+
+        if ($("#start-year").val().trim()) {
+            url += "&begin_date="+$("#start-year").val().trim();
+        }
+
+
+        console.log(url);
 
 
         $.ajax({
@@ -36,18 +54,28 @@ $(document).ready(function () {
             console.log(result);
             console.log(result.response.docs);
             //var resultSearch = result.response.docs[i].byline;
-   
 
 
-            for (i = 0; i <= limit; i++) {
 
-               
+            for (i = 0; i <= limit - 1; i++) {
+
+
 
                 var newsDiv = $("<div>");
-                var button = $("<button>").text(i+1);
-                console.log(result.response.docs[i].headline.main);
-                var title = $("<h3>").text(result.response.docs[i].headline.main);
-                var author = $("<p>").text("author : " + result.response.docs[i].byline.original);
+                var button = $("<button>").text(i + 1);
+               
+                if (result.response.docs[i].headline){
+                    var title = $("<h3>").text(result.response.docs[i].headline.main);
+                } else {
+                    var title = $("<h3>").text("No title");
+                }
+
+                
+                if (result.response.docs[i].byline){
+                    var author = $("<p>").text("Author : " + result.response.docs[i].byline.original);
+                } else {
+                    var author = $("<p>").text("Author : no author");
+                }
 
 
                 newsDiv.append(button);
@@ -58,7 +86,7 @@ $(document).ready(function () {
                 $("#show-result").append(newsDiv);
             }
 
-          
+
 
         }).catch(function (err) {
             throw err;
